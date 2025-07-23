babf6b3a7ce8276ec32fa798eefcd734302fef86
Marvin Cai <cai19930303@gmail.com>
2025-07-23 22:13:46 +0800

    Generate authentication related code and add username

--- .iex.exs
+++ .iex.exs
@@ -3,6 +3,6 @@ alias PhoenixRaffley.Raffles
 alias PhoenixRaffley.RaffleAdmin
 alias PhoenixRaffley.Raffles.Raffle
 alias PhoenixRaffley.Charities.Charity
-
+alias PhoenixRaffley.Accounts.User
 import Ecto.Query
 import Ecto.Changeset
--- lib/phoenix_raffley_web/router.ex
+++ lib/phoenix_raffley_web/router.ex
@@ -1,6 +1,8 @@
 defmodule PhoenixRaffleyWeb.Router do
   use PhoenixRaffleyWeb, :router
 
+  import PhoenixRaffleyWeb.UserAuth
+
   pipeline :browser do
     plug :accepts, ["html"]
     plug :fetch_session
@@ -8,6 +10,7 @@ defmodule PhoenixRaffleyWeb.Router do
     plug :put_root_layout, html: {PhoenixRaffleyWeb.Layouts, :root}
     plug :protect_from_forgery
     plug :put_secure_browser_headers
+    plug :fetch_current_user
     plug :spy
   end
 
@@ -18,13 +21,12 @@ defmodule PhoenixRaffleyWeb.Router do
   def spy(conn, _opts) do
     greeting = ~w(Hi Howdy Hellow) |> Enum.random()
     conn = assign(conn, :greeting, greeting)
-    # IO.inspect(conn, label: "Request Details")
+    IO.inspect(conn, label: "Request Details")
     conn
   end
 
   scope "/", PhoenixRaffleyWeb do
     pipe_through :browser
-
     #get "/", PageController, :home
     get "/rules", RuleController, :index
     get "/rules/:id", RuleController, :show
@@ -33,7 +35,10 @@ defmodule PhoenixRaffleyWeb.Router do
     live "/estimator", EstimatorLive
     live "/raffles", RaffleLive.Index
     live "/raffles/:id", RaffleLive.Show
+  end
 
+  scope "/", PhoenixRaffleyWeb do
+    pipe_through [:browser, :require_authenticated_user]
     live "/admin/raffles", AdminRaffleLive.Index
     live "/admin/raffles/new", AdminRaffleLive.Form, :new
     live "/admin/raffles/:id/edit", AdminRaffleLive.Form, :edit
@@ -69,6 +74,44 @@ defmodule PhoenixRaffleyWeb.Router do
 
       live_dashboard "/dashboard", metrics: PhoenixRaffleyWeb.Telemetry
       forward "/mailbox", Plug.Swoosh.MailboxPreview
+    end
+  end
+
+  ## Authentication routes
+
+  scope "/", PhoenixRaffleyWeb do
+    pipe_through [:browser, :redirect_if_user_is_authenticated]
+
+    live_session :redirect_if_user_is_authenticated,
+      on_mount: [{PhoenixRaffleyWeb.UserAuth, :redirect_if_user_is_authenticated}] do
+      live "/users/register", UserLive.Registration, :new
+      live "/users/log-in", UserLive.Login, :new
+      live "/users/reset-password", UserLive.ForgotPassword, :new
+      live "/users/reset-password/:token", UserLive.ResetPassword, :edit
+    end
+
+    post "/users/log-in", UserSessionController, :create
+  end
+
+  scope "/", PhoenixRaffleyWeb do
+    pipe_through [:browser, :require_authenticated_user]
+
+    live_session :require_authenticated_user,
+      on_mount: [{PhoenixRaffleyWeb.UserAuth, :ensure_authenticated}] do
+      live "/users/settings", UserLive.Settings, :edit
+      live "/users/settings/confirm-email/:token", UserLive.Settings, :confirm_email
+    end
+  end
+
+  scope "/", PhoenixRaffleyWeb do
+    pipe_through [:browser]
+
+    delete "/users/log-out", UserSessionController, :delete
+
+    live_session :current_user,
+      on_mount: [{PhoenixRaffleyWeb.UserAuth, :mount_current_user}] do
+      live "/users/confirm/:token", UserLive.Confirmation, :edit
+      live "/users/confirm", UserLive.ConfirmationInstructions, :new
     end
   end
 end
